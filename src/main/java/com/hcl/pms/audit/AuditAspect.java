031fa8acfac85d518d909ad85a31d9c8d1056d9d
PiyumG <pg.piyum.guruge@gmail.com>
2022-03-26 16:41:17 +0530

    Audit aspect changes

--- src/main/java/com/hcl/pms/audit/AuditAspect.java
+++ src/main/java/com/hcl/pms/audit/AuditAspect.java
@@ -1,10 +1,12 @@
 package com.hcl.pms.audit;
 
+import com.hcl.pms.dto.OrderEntryDto;
 import com.hcl.pms.model.AuditAction;
 import com.hcl.pms.repository.AuditActionRepository;
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
+import java.util.function.Function;
 
 @Aspect
 @Component
@@ -21,14 +24,16 @@ public class AuditAspect {
 
     private static final Logger LOG = Logger.getLogger(AuditAspect.class);
 
+    Function<JoinPoint, String> getClassName = joinPoint -> Optional.ofNullable(joinPoint.getSignature()).isPresent()
+            ? joinPoint.getSignature().getName() : null;
+
     private AuditAspect(){
     }
 
-    @Before("execution(* com.hcl.pms.ctrl.*.*(..)) && target(request)")
+    @Before("execution(* com.hcl.pms.ctrl.*.createOrderEntry(..)) && target(request)")
     public void audit(JoinPoint joinPoint, HttpServletRequest request){
 
-        String methodSignature = Optional.ofNullable(joinPoint.getSignature()).isPresent()
-                        ? joinPoint.getSignature().getName() : null;
+        String methodSignature = getClassName.apply(joinPoint);
 
         String loggedUser = Optional.ofNullable(request).isPresent() ? request.getRemoteUser() : null;
         String incomingData = methodSignature + " - " + loggedUser;
@@ -43,11 +48,12 @@ public class AuditAspect {
         }
     }
 
-    /*@After(value = "execution(* com.hcl.pms.ctrl.*.*(..)) && target(response)")
-    public static void audit(JoinPoint joinPoint, HttpServletResponse response){
+/*    @AfterReturning(value = "execution(* com.hcl.pms.ctrl.*.createOrderEntry(..))", returning = "result")
+    public void audit(JoinPoint joinPoint, Object result){
 
-        String methodSignature = Optional.ofNullable(joinPoint.getSignature()).isPresent()
-                ? joinPoint.getSignature().getName() : null;
+        String methodSignature = getClassName.apply(joinPoint);
+
+        //(OrderEntryDto)result
 
     }*/
 

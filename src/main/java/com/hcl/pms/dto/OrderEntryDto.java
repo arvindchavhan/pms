b30324d9ccbca4957ddee1d2c89e41be7ee12d0a
Arvind <arvind.chavhan25@gmail.com>
2022-03-26 15:58:41 +0800

    Order entry

--- src/main/java/com/hcl/pms/dto/OrderEntryDto.java
+++ src/main/java/com/hcl/pms/dto/OrderEntryDto.java
@@ -1,6 +1,17 @@
 package com.hcl.pms.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class OrderEntryDto {
     
+    private String fundName;
+
+    private String transactionType;
+
+    private Integer quantity;
+
+    private Double orderValue;
 }
